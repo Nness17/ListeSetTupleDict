c0e9221f1c229a57dcb13ce653d174a4558269d7
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-03-15 12:32:31 +0100

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -15,22 +15,32 @@ colors = {"rosso": "#FF0000", "verde": "#00FF00", "blu": "#0000FF"}
 # Definiamo l'interfaccia utente di Streamlit
 st.write("# Utilizzo di liste, tuple, set e dizionari in Python")
 
+# Creiamo una barra laterale per scegliere cosa stampare
+selected_option = st.sidebar.selectbox(
+    "Cosa vuoi stampare?",
+    ("Frutti", "Città", "Numeri", "Colori")
+)
+
 # Mostriamo la lista di frutti
-st.write("### Lista di frutti")
-for fruit in fruits:
-    st.write(fruit)
+if selected_option == "Frutti":
+    st.write("### Lista di frutti")
+    for fruit in fruits:
+        st.write(fruit)
 
 # Mostriamo la tupla di città
-st.write("### Tupla di città")
-for city in cities:
-    st.write(city)
+elif selected_option == "Città":
+    st.write("### Tupla di città")
+    for city in cities:
+        st.write(city)
 
 # Mostriamo il set di numeri
-st.write("### Set di numeri")
-for number in numbers:
-    st.write(number)
+elif selected_option == "Numeri":
+    st.write("### Set di numeri")
+    for number in numbers:
+        st.write(number)
 
 # Mostriamo il dizionario di colori
-st.write("### Dizionario di colori")
-for key, value in colors.items():
-    st.write(f"{key}: {value}")
+else:
+    st.write("### Dizionario di colori")
+    for key, value in colors.items():
+        st.write(f"{key}: {value}")
