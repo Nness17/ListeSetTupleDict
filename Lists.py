16352f031793fa608e049bff7aa6a9db05f68871
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-03-15 12:36:57 +0100

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,46 +1,44 @@
 import streamlit as st
+from IPython.display import HTML
 
-# Creiamo una lista di frutti
-fruits = ["mela", "banana", "kiwi", "ananas", "arancia"]
-
-# Creiamo una tupla di città
-cities = ("Roma", "Milano", "Napoli", "Torino", "Firenze")
-
-# Creiamo un set di numeri
-numbers = {1, 2, 3, 4, 5}
-
-# Creiamo un dizionario di colori
-colors = {"rosso": "#FF0000", "verde": "#00FF00", "blu": "#0000FF"}
+# Definiamo i generi musicali e le rispettive canzoni
+music_genres = {
+    "Pop": {
+        "Dua Lipa - Levitating": "https://www.youtube.com/watch?v=TUVcZfQe-Kw",
+        "Ariana Grande - positions": "https://www.youtube.com/watch?v=t9-iLYhyIgg",
+        "Ed Sheeran - Shape of You": "https://www.youtube.com/watch?v=JGwWNGJdvx8"
+    },
+    "Rock": {
+        "Queen - Bohemian Rhapsody": "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
+        "The Rolling Stones - (I Can't Get No) Satisfaction": "https://www.youtube.com/watch?v=nrIPxlFzDi0",
+        "Led Zeppelin - Stairway to Heaven": "https://www.youtube.com/watch?v=9Q7Vr3yQYWQ"
+    },
+    "Hip Hop": {
+        "Kendrick Lamar - HUMBLE.": "https://www.youtube.com/watch?v=tvTRZJ-4EyI",
+        "Drake - In My Feelings": "https://www.youtube.com/watch?v=DRS_PpOrUZ4",
+        "Notorious B.I.G. - Juicy": "https://www.youtube.com/watch?v=_JZom_gVfuw"
+    }
+}
 
 # Definiamo l'interfaccia utente di Streamlit
-st.write("# Utilizzo di liste, tuple, set e dizionari in Python")
+st.write("# Generi musicali")
 
-# Creiamo una barra laterale per scegliere cosa stampare
-selected_option = st.sidebar.selectbox(
-    "Cosa vuoi stampare?",
-    ("Frutti", "Città", "Numeri", "Colori")
+# Creiamo una barra laterale per scegliere il genere musicale
+selected_genre = st.sidebar.selectbox(
+    "Scegli il genere musicale",
+    list(music_genres.keys())
 )
 
-# Mostriamo la lista di frutti
-if selected_option == "Frutti":
-    st.write("### Lista di frutti")
-    for fruit in fruits:
-        st.write(fruit)
-
-# Mostriamo la tupla di città
-elif selected_option == "Città":
-    st.write("### Tupla di città")
-    for city in cities:
-        st.write(city)
-
-# Mostriamo il set di numeri
-elif selected_option == "Numeri":
-    st.write("### Set di numeri")
-    for number in numbers:
-        st.write(number)
-
-# Mostriamo il dizionario di colori
+# Mostrare la canzone selezionata
+if selected_genre in music_genres:
+    st.write(f"## Canzoni {selected_genre}")
+    selected_song = st.selectbox(
+        "Scegli la canzone",
+        list(music_genres[selected_genre].keys())
+    )
+    selected_song_url = music_genres[selected_genre][selected_song]
+    st.write(f"### {selected_song}")
+    st.write(f"Guarda il video su [YouTube]({selected_song_url})")
+    HTML(f'<iframe width="560" height="315" src="{selected_song_url}" frameborder="0" allowfullscreen></iframe>')
 else:
-    st.write("### Dizionario di colori")
-    for key, value in colors.items():
-        st.write(f"{key}: {value}")
+    st.write("Genere musicale non trovato")
