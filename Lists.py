84cdb009a6340540ab6014b4d80a93772acac682
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-03-15 13:06:16 +0100

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,45 +1,47 @@
 import streamlit as st
 
-# Definisci i generi musicali e le loro canzoni
-generi_musicali = {
-    'Rock': [
-        ('Queen', 'Bohemian Rhapsody', 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ'),
-        ('Led Zeppelin', 'Stairway to Heaven', 'https://www.youtube.com/watch?v=9Q7Vr3yQYWQ'),
-        ('Pink Floyd', 'Comfortably Numb', 'https://www.youtube.com/watch?v=_FrOQC-zEog')
-    ],
-    'Pop': {
-        ('Michael Jackson', 'Thriller', 'https://www.youtube.com/watch?v=4V90AmXnguw'),
-        ('Madonna', 'Like a Virgin', 'https://www.youtube.com/watch?v=s__rX_WL100'),
-        ('Whitney Houston', 'I Will Always Love You', 'https://www.youtube.com/watch?v=3JWTaaS7LdU')
+# Definiamo i generi musicali e le rispettive canzoni
+music_genres = {
+    "Pop": {
+        "Dua Lipa - Levitating": "https://www.youtube.com/watch?v=TUVcZfQe-Kw",
+        "Ariana Grande - positions": "https://www.youtube.com/watch?v=t9-iLYhyIgg",
+        "Ed Sheeran - Shape of You": "https://www.youtube.com/watch?v=JGwWNGJdvx8"
     },
-    'Hip Hop': {
-        ('Tupac', 'California Love', 'https://www.youtube.com/watch?v=5wBTdfAkqGU'),
-        ('Notorious B.I.G.', 'Juicy', 'https://www.youtube.com/watch?v=_JZom_gVfuw'),
-        ('Nas', 'NY State of Mind', 'https://www.youtube.com/watch?v=UKjj4hk0pV4')
-    }
+    "Rock": {
+        "Queen - Bohemian Rhapsody": "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
+        "The Rolling Stones - (I Can't Get No) Satisfaction": "https://www.youtube.com/watch?v=nrIPxlFzDi0",
+        "Led Zeppelin - Stairway to Heaven": "https://www.youtube.com/watch?v=9Q7Vr3yQYWQ"
+    },
+    "Hip Hop": {
+        "Kendrick Lamar - HUMBLE.": "https://www.youtube.com/watch?v=tvTRZJ-4EyI",
+        "Drake - In My Feelings": "https://www.youtube.com/watch?v=DRS_PpOrUZ4",
+        "Notorious B.I.G. - Juicy": "https://www.youtube.com/watch?v=_JZom_gVfuw"
+    },
+    "Italian Rap": {
+        "OUV3RTURE - Lazza": "https://www.youtube.com/watch?v=TTxqCWGBYhg",
+        "Charlie Charles - Bimbi ft. Izi, Rkomi, Sfera Ebbasta, Tedua, Ghali": "https://youtu.be/l9WWa98S4OM",
+        
+    }     
 }
 
-# Crea una lista con tutti i nomi delle canzoni utilizzando un set comprehension
-canzoni = {canzone for genere in generi_musicali for artista, canzone, link in generi_musicali[genere]}
+# Definiamo l'interfaccia utente di Streamlit
+st.write("#ITALIAN MUSIC")
 
-# Definisci la barra laterale per selezionare il genere musicale
-genere_selezionato = st.sidebar.selectbox(
-    'Seleziona il genere musicale',
-    list(generi_musicali.keys())
+# Creiamo una barra laterale per scegliere il genere musicale
+selected_genre = st.sidebar.selectbox(
+    "Select the musical Genre you want to listen to: ",
+    list(music_genres.keys())
 )
 
-# Crea una lista con tutte le canzoni del genere selezionato utilizzando una tuple e list comprehension
-canzoni_genere_selezionato = [canzone[1] for canzone in generi_musicali[genere_selezionato]]
-
-# Definisci la sezione principale della pagina
-st.title('Generi musicali')
-st.write(f'Selezionato: {genere_selezionato}')
-st.write(f'Canzoni: {", ".join(canzoni_genere_selezionato)}')
-
-# Crea una lista con tutte le canzoni del genere selezionato e i link a YouTube utilizzando una lista comprehension
-canzoni_e_link = [f'{canzone} - {artista} ({link})' for artista, canzone, link in generi_musicali[genere_selezionato]]
-
-# Mostra la lista di canzoni e link nella sezione principale
-st.write('Canzoni e link a YouTube:')
-for canzone in canzoni_e_link:
-    st.write(canzone)
+# Mostrare la canzone selezionata
+if selected_genre in music_genres:
+    st.write(f"## {selected_genre}")
+    selected_song = st.selectbox(
+        "Select the song",
+        list(music_genres[selected_genre].keys())
+    )
+    selected_song_url = music_genres[selected_genre][selected_song]
+    st.write(f"### {selected_song}")
+    st.write(f"Listen the song on [YouTube]({selected_song_url})")
+else:
+    st.write("Music genre not found :(")
