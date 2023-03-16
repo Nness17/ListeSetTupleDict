3925cfc162f9df1a8afd5a3f0d3b686115d13588
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-03-16 11:58:06 +0100

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -2,12 +2,19 @@ import streamlit as st
 
 # Definiamo i generi musicali e le rispettive canzoni
 music_genres = {
+     "Salmo": {
+        "S.A.L.M.O. - Salmo": "https://youtu.be/_knSEmsWJ6I",
+        "STAI ZITTO ft. Fabri Fibra - Salmo": "https://youtu.be/XqEIwnDY92g",
+        "Russell Crowe ft. CYBERPUNKERS - Salmo": "https://youtu.be/LiTcL3owCHQ",
+        "ANTIPATICO - Salmo": "https://youtu.be/tIJa5miGWCg",
+        "90MIN - Salmo": "https://youtu.be/7LmSC66Ia9s"
+    },
     "Rosa Chemical": {
         "Made In Italy (Sanremo 2023) prod. BDope - Rosa Chemical": "https://youtu.be/UQuoY73tUsw",
         "Polka 3 prod. BDope - Rosa Chemical": "https://youtu.be/vu8DDbY0jZA",
         "britney ;-) ft. MamboLosco, Radical - Rosa Chemical": "https://youtu.be/76Zny0XaV6Q",
-        "LA CANZONE NOSTRA ft. MACE, Salmo - BLANCO": "https://youtu.be/XE6lV6xseQ4",
-        "L'Isola delle Rose - BLANCO": "https://youtu.be/g2647z7McDE"
+        "non è normale - Rosa Chemical": "https://youtu.be/u2IhJlCusNk",
+        "POLKA prod. Thelonious B. - Rosa Chemical": "https://youtu.be/hhOdwmnHzM8"
     },
     "BLANCO": {
         "Notti in Bianco - BLANCO": "https://youtu.be/wppn2Via-Tk",
@@ -87,15 +94,28 @@ music_genres = {
         "J - Lazza": "https://youtu.be/vgZoXnrN_eI",
         "Alibi - Lazza": "https://youtu.be/vzHuYaS6Iio",
         "SENZA RUMORE - Lazza": "https://youtu.be/dJmaFREMVnw"
-    }     
+    },
+     "Caparezza": {
+        "Avrai Ragione Tu - Caparezza": "https://youtu.be/a9tf49drPKY",
+        "Fuori dal Tunnel - Caparezza": "https://youtu.be/jx8GhXm-HcA",
+        "Non me lo posso permettere - Caparezza": "https://youtu.be/umUmQgkgFT8",
+        "La Scelta - Caparezza": "https://youtu.be/D8ZVhvXqUzI",
+        "Ti Fa Stare Bene - Caparezza": "https://youtu.be/84rgsCJOMUA"
+    },   
+    "Featuring+": {
+        "Charles Manson (BUON NATALE 2) ft. Dani Faiv, Lazza, Nitro - Salmo": "https://youtu.be/0fxxis1mx94",
+        "Bimbi ft. Izi, Tedua, Rkomi, Sfera Ebbasta, Ghali - Charlie Charles": "https://youtu.be/l9WWa98S4OM",
+        "Battle Royale MM3 #20 ft. EL Raton, Bassi Maestro, Rasty Kilo, Madman, En?gma, Noyz Narcos, Jack The Smoker, Salmo, Rocco Hunt, Gemitaiz, Nitro - Machete Tv": "https://youtu.be/dvRoT9Of05U",
+        "King's Supreme ft. El Raton, Ensi, Salmo, En?gma, Bassi M., R. Hunt, Gemitaiz  - Machete Tv": "https://youtu.be/thiawcUa-Js"
+    } 
 }
 
 # Definiamo l'interfaccia utente di Streamlit
-st.write(f"##ITALIAN MUSIC")
+st.write(f"ITALIAN SCENE")
 
 # Creiamo una barra laterale per scegliere il genere musicale
 selected_genre = st.sidebar.selectbox(
-    "Select the musical Genre you want to listen to: ",
+    "Select the artist you want to listen to: ",
     list(music_genres.keys())
 )
 
@@ -110,4 +130,4 @@ if selected_genre in music_genres:
     st.write(f"### {selected_song}")
     st.write(f"Listen the song on [YouTube]({selected_song_url})")
 else:
-    st.write("Music genre not found :(")
+    st.write("Music not found :(")
