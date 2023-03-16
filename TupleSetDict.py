d44afca92718427442968827177aebef3092d780
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-03-16 12:33:28 +0100

    Update TupleSetDict.py

--- TupleSetDict.py
+++ TupleSetDict.py
@@ -10,7 +10,7 @@ def count_words(text):
     word_count = {}
     for word in words:
         if word not in word_count:
-            word_count[word] += 1
+            word_count[word] = 1
         else:
             word_count[word] += 1
     return word_count
